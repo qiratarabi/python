01321d722516a30e91eaf892d2de936b2d3c67af
qiratarabi <anu_karim@hotmail.com>
2025-07-13 15:35:46 +0500

    done with day 1 code

---
+++ day1/main.py
@@ -0,0 +1,14 @@
+print("qirat") 
+print(2+3)
+print("qirat "*5)
+name="qirat"
+age=10+3
+print(name)
+print(age)
+name=input("write your name")
+print(name)
+
+
+import keyword
+print(keyword.kwlist)
+
