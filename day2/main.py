8bbb66eb1bef0b87319ecb55a35b841bb61cb00a
qiratarabi <anu_karim@hotmail.com>
2025-07-14 20:15:56 +0500

    day 2 datatypes done

---
+++ day2/main.py
@@ -0,0 +1,28 @@
+#Int - whole numbers -123, 12, 0, 23
+#Float - decimal point number 12.123, -0.556, 10.00
+#Char - helps to store alphabets or special chracters a,@
+#String/objects-collection of characters like a sentence or name
+#Boolean - stores trueor false 
+a = 10
+print(type(a))
+b = 10.234
+print(type(b))
+c = 'a'
+print(type(c))
+d = "Qirat"
+print(type(d))
+e = True
+print(type(e))
+
+# Convertion between data types converting one datatype to another
+# a character or string if it is not a number cant be converterted to a int or float
+# int can be converted float and vise - versa
+# boolean can be converted tointand int can be converted to boolean
+f = "123456.234"
+print(type(f))
+e = float(e)
+print(type(e))
+a = "10"
+b = "20"
+sum = a + b 
+print(sum)
