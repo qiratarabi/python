3a51a11f31c5c543fa16b61d02c5ece809b25494
qiratarabi <anu_karim@hotmail.com>
2025-08-01 19:52:00 +0500

    done day9

---
+++ day9/ac2.py
@@ -0,0 +1,7 @@
+string = input("Please enter your own string")
+string2 = ('') #QIRAT
+for i in string:
+    string2 = i + string2
+
+print("The orignial string: ", string)
+print("The reversed string: ", string2)
---
+++ day9/ac3.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the value of n:"))
+print(f"numbers from {n} to 1")
+for i in range (n, 0, -1):
+    print(i)
+    
